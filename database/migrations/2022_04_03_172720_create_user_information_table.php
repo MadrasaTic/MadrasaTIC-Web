b523d7589a27a42070e35edb2240864da10030a0
Yassine <ya.latreche@esi-sba.dz>
2022-04-03 18:47:53 +0100

    position and userInformation created

---
+++ database/migrations/2022_04_03_172720_create_user_information_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_informations', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number');
+            $table->string('avatar_path');
+            $table->unsignedBigInteger('position_id');
+            $table->timestamps();
+            $table->primary('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_informations');
+    }
+}
