cfc1348189c60f5b5b8519167797959a667dc3d7
habs <abenchikh6@gmail.com>
2022-04-05 15:59:50 +0100

    Added testing UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'testing',
+            'email' => 'test@test.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
