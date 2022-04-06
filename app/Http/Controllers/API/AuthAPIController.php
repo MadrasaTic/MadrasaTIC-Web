92b79962ef6b763f88f2c0b07bda170be2c096cb
Yassine <ya.latreche@esi-sba.dz>
2022-04-06 01:34:01 +0100

    Updating AuthAPI with picture upload

--- app/Http/Controllers/API/AuthAPIController.php
+++ app/Http/Controllers/API/AuthAPIController.php
@@ -57,17 +57,38 @@ class AuthAPIController extends Controller
                 'email_verified_at' => now(),
                 'name' => $user->getName(),
                 'status' => true,
+                'activated' => true,
+                'google_id' => $user->getId(),
             ]
         );
+        // dd($userCreated);
+        $userCreated->userInformation()->updateOrCreate(
+            [
+                'user_id' => $userCreated->id
+            ],
+            [
+                'first_name' => $user->offsetGet('given_name'),
+                'last_name' => $user->offsetGet('family_name'),
+                'avatar_path' => getSocialAvatar(
+                    str_replace('-w=s96', '-w=s500', $user->getAvatar()), $user),
+                'position_id' => 1,
+                'google_id' => $user->getId(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+
         $userCreated->providers()->updateOrCreate(
             [
                 'provider' => $provider,
                 'provider_id' => $user->getId(),
             ],
             [
-                'avatar' => $user->getAvatar()
+                'avatar' => getSocialAvatar(
+                    str_replace('-w=s96', '-w=s500', $user->getAvatar()), $user),
             ]
         );
+        dd($user->token);
         $token = $userCreated->createToken('token-name')->plainTextToken;
 
         return response()->json($userCreated, 200, ['Access-Token' => $token]);
@@ -82,6 +103,75 @@ class AuthAPIController extends Controller
         if (!in_array($provider, ['facebook', 'github', 'google'])) {
             return response()->json(['error' => 'Please login using facebook, github or google'], 422);
         }
+    }
+
+       // login not using a provider
+       protected function requestToken(Request $request){
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        // getting user
+        $user = User::where('email', $request->email)->first();
+
+        // checking credentials
+        if( !$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessage([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        // Returning response
+        return response()->json($this->getUserAndToken($user, $request->device_name));
+
+    }
+
+    protected function requestTokenGoogle(Request $request){
+        // Getting the user from socialite using token from google
+        // dd($request);
+        $user = Socialite::driver('google')->stateless()->userFromToken($request->token);
+        // Getting or creating user from db
+        $userCreated = User::firstOrCreate(
+            [
+                'email' => $user->getEmail()
+            ],
+            [
+                'email_verified_at' => now(),
+                'name' => $user->getName(),
+                'status' => true,
+                'activated' => true,
+                'google_id' => $user->getId(),
+            ]
+        );
+        // dd($userCreated);
+        $userCreated->userInformation()->updateOrCreate(
+            [
+                'user_id' => $userCreated->id
+            ],
+            [
+                'first_name' => $user->offsetGet('given_name'),
+                'last_name' => $user->offsetGet('family_name'),
+                'avatar_path' => getSocialAvatar(str_replace('-w=s96', '-w=s500', $user->getAvatar()), $user),
+                'position_id' => 1,
+                'google_id' => $user->getId(),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+
+        // Returning response
+        return response()->json($this->getUserAndToken($userCreated));
+    }
+
+    //region Helpers
+
+    private function getUserAndToken(User $user, $device_name){
+        return [
+            'User' => $user,
+            'Access-Token' => $user->createToken($device_name)->plainTextToken,
+        ];
     }
 
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,8 @@
 <?php
 
-function getSocialAvatar($file, $path, $user){
-    $fileContents = file_get_contents($file);
-    \File::put(public_path() . '/' . $path . $user->getId() . ".jpg", $fileContents);
-    return public_path($path . $user->getId() . ".jpg");
+function getSocialAvatar($file, $user){
+    $profilePicture = file_get_contents($file);
+    $filename = $user->getId() . ".jpg";
+    Storage::disk('public')->put('images/'.$filename, $profilePicture);
+    return $filename;
 }
