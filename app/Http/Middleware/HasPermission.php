e01f9952f066abc922148c741f01d49cab912086
Yassine <ya.latreche@esi-sba.dz>
2022-04-06 15:49:58 +0100

    Updating routes and permisisons handling

--- app/Http/Middleware/HasPermission.php
+++ app/Http/Middleware/HasPermission.php
@@ -16,24 +16,23 @@ class HasPermission
         $_exist_role_user = \DB::table('role_user')
                         ->where('user_id',auth()->id())
                         ->exists();
-        
-        if( $_exist_role_user ) {
 
+        if( $_exist_role_user ) {
             $user_role = \DB::table('role_user')
                             ->where('user_id',auth()->id())
                             ->first();
 
             $controllerAction = class_basename(Route::currentRouteAction());
-    
+
             $permission = false;
             $permission_id_id = 0;
 
             $permission_result = Permission::where('name',$controllerAction)
                                     ->exists();
-            
+
             $permission_id = Permission::where('name',$controllerAction)
                                 ->first();
-    
+
             if(!$permission_result) {
                 $permission_id_id = 0;
                 return $next($request);
@@ -41,29 +40,29 @@ class HasPermission
             else {
                 $permission_id_id = $permission_id->id;
             }
-            
+
             $role_id = $user_role->role_id;
-    
+
             $check_permission = \DB::table('permission_role')
                                 ->where('role_id',$role_id)
                                 ->where('permission_id',$permission_id_id)
                                 ->exists();
-    
+
             if($check_permission) {
-    
+
                 $permission = true;
-    
+
                 if($permission) {
                     return $next($request);
                 }
-    
+
                 return abort(403);
 
             }else {
                 return abort(403);
             }
         }else {
-            return 'you are not authorize!';
+            return abort(403);
         }
     }
 }
--- resources/views/include/sideBar.blade.php
+++ resources/views/include/sideBar.blade.php
@@ -4,30 +4,30 @@
 </head>
 
 
-<div class="col d-flex align-items-start justify-content-center " id="sidebar--container"> 
+<div class="col d-flex align-items-start justify-content-center " id="sidebar--container">
     <div id="sidebar--top" class="d-flex align-items-center justify-content-center border-danger" >
             <div class="h-auto w-100 ">
                 <h3 class="text-primary fw-bold">Menu</h3>
                 <div name="qsdf" class="h-auto w-100 h4 mt-4  sidebar_links">
-                    <span name=""><i class="fa-solid fa-warning p-3 ms-3 me-1"></i>Signalement</span>
+                    <a href="#"><span name=""><i class="fa-solid fa-warning p-3 ms-3 me-1"></i>Signalement</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links">
-                    <span name=""><i class="fa-solid fa-bullhorn p-3 ms-3 me-1"></i>Annonce</span>
+                    <a href="#"><span name=""><i class="fa-solid fa-bullhorn p-3 ms-3 me-1"></i>Annonce</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links">
-                    <span name=""><i class="fa-solid fa-save p-3 ms-3 me-1"></i>Enregistré</span>
+                    <a href="#"><span name=""><i class="fa-solid fa-save p-3 ms-3 me-1"></i>Enregistré</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links" id="roles">
-                    <span name="roles"><i class="fa-solid fa-key p-3 ms-3 me-1"></i>Roles</span>
+                    <a href="{{ route('roles') }}"><span name="roles"><i class="fa-solid fa-key p-3 ms-3 me-1"></i>Roles</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links" id="permissions">
-                    <span name="permissions"><i class="fa-regular fa-star p-3 ms-3 me-1"></i>Permissions</span>
+                    <a href="{{ route('permissions') }}"><span name="permissions"><i class="fa-regular fa-star p-3 ms-3 me-1"></i>Permissions</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links" id="members">
-                    <span name="members"><i class="fa-solid fa-users p-3 ms-3 me-1"></i>Membres</span>
+                    <a href="{{ route('members') }}"><span name="members"><i class="fa-solid fa-users p-3 ms-3 me-1"></i>Membres</span></a>
                 </div>
                 <div class="h-auto w-100  h4 sidebar_links" id="profile">
-                    <span name="profile"><i class="fa-solid fa-user p-3 ms-3 me-1"></i>Mon Profil</span>
+                    <a href="{{ route('profile') }}"><span name="profile"><i class="fa-solid fa-user p-3 ms-3 me-1"></i>Mon Profil</span></a>
                 </div>
             </div>
     </div>
