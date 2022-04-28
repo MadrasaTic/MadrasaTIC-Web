4ee17b2be8bfbf50aefe5d0780807a3cd3afc589
habs <abenchikh6@gmail.com>
2022-04-28 21:17:48 +0100

    Added select list, data ids, and other minor changes

--- resources/views/include/categoryFormModify.blade.php
+++ resources/views/include/categoryFormModify.blade.php
@@ -1,14 +1,15 @@
-<form class="row" id="modal_update--form" method="POST" action="/members">
+<form class="row" id="modal_update--form" method="POST" action="/signalmentsCategory">
+    @csrf
     <div class="mb-4 col-md-12 d-none">
         <label class="form-label">ID</label>
         <div class="input-group">
-            <input type="text" class="w-100 p-3" placeholder="ID" />
+            <input type="text" class="w-100 p-3" placeholder="ID" name="id" id="id"/>
         </div>
     </div>
     <div class="mb-4 col-md-12">
         <label class="form-label">Nom</label>
         <div class="input-group">
-            <input type="text" class="needs--validation w-100 p-3" placeholder="Nom" />
+            <input type="text" class="needs--validation w-100 p-3" placeholder="Nom" name="name" id="name"/>
             <span class="check--container end-0 me-2 fs-4 ">
                 <i
                     class="valid--icon fa-solid fa-circle-check text-success d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
@@ -20,38 +21,35 @@
         <div class="valid-feedback fs-6 d-none">Nom valide</div>
     </div>
     <div class="mb-4 col-md-12">
-        <label class="form-label">Catégorie Parent</label>
+        <label for="parent_id" class="form-label">Catégorie Parent</label>
         <div class="input-group">
-            <input type="text" class="needs--validation w-100 p-3" placeholder="Catégorie Parent" />
-            <span class="check--container end-0 me-2 fs-4 ">
-                <i
-                    class="valid--icon fa-solid fa-circle-check text-success d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
-                <i
-                    class="invalid--icon  fa-solid fa-circle-exclamation text-danger d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
-            </span>
+            <select name="parent_id" class="w-100 p-3" id="parent_id">
+                <option selected disabled></option>
+                @foreach($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
         </div>
-        <div class="invalid-feedback fs-6 d-none">Enter un nom valide</div>
-        <div class="valid-feedback fs-6 d-none">Nom valide</div>
     </div>
     <div class="mb-4 col-md-12">
         <label for="last_name" class="form-label">Priorité par défaut</label>
         <div class="input-group">
-            <input type="number" class="w-100 p-3" placeholder="Priorité par défaut" />
+            <input type="number" class="w-100 p-3 needs--validation" placeholder="Priorité par défaut" name ="priority_default" id="priority_default" min="0"/>
             <span class="check--container end-0 me-2 fs-4 ">
                 <i
                     class="valid--icon fa-solid fa-circle-check text-success d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
                 <i
-                    class="invalid--icon  fa-solid fa-circle-exclamation text-danger d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
+                    class="invalid--icon fa-solid fa-circle-exclamation text-danger d-none animateanimated animatefadeIn animate__delay-0.5s"></i>
             </span>
         </div>
-        <div class="invalid-feedback fs-6 d-none">Enter un nom valide</div>
-        <div class="valid-feedback fs-6 d-none">Nom valide</div>
+        <div class="invalid-feedback fs-6 d-none">Enter un numéro valide</div>
+        <div class="valid-feedback fs-6 d-none">Numéro valide</div>
     </div>
     <div class="mb-4 col-md-12">
-        <label for="last_name" class="form-label">Déscription</label>
+        <label for="last_name" class="form-label">Description</label>
         <div class="input-group">
-            <textarea class="w-100 p-3" placeholder="Déscription du Service" style="height: 25vh"
-                name="description"></textarea>
+            <textarea class="w-100 p-3 text--area" placeholder="Description de la catégorie" style="height: 25vh"
+                name="description" id="description"></textarea>
         </div>
     </div>
     <input type="submit" value="ICI" class="modify--submit" hidden>
