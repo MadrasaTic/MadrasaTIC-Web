95568ec26cf56ff1b44b3fcbefccce8ba12035d5
habs <abenchikh6@gmail.com>
2022-04-28 21:14:48 +0100

    added create_categories_table migration

--- database/migrations/2022_04_28_160023_create_categories_table.php
+++ database/migrations/2022_04_28_160023_create_categories_table.php
@@ -15,6 +15,11 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('parent_id')->nullable();
+            $table->integer('priority_default');
+            $table->text('description')->nullable();
+            $table->integer('service_id')->nullable();
             $table->timestamps();
         });
     }
