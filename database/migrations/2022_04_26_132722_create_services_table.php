5fddcc54f7c5211325370c5fb3bb80b5d4bd56d3
habs <abenchikh6@gmail.com>
2022-04-29 03:41:58 +0100

    Added id to migration

--- database/migrations/2022_04_26_132722_create_services_table.php
+++ database/migrations/2022_04_26_132722_create_services_table.php
@@ -16,7 +16,7 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('responsable')->nullable();
+            $table->integer('responsable_id')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
         });
