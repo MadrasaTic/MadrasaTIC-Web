1bb0002c79769b4cb76a3ef042cef83f22e0bae3
habs <abenchikh6@gmail.com>
2022-04-29 03:41:43 +0100

    Added user to service relationship

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -5,16 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
+use App\Models\User;
 
 class Service extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'responsable_id'];
     
     public function category()
     {
     return $this->belongsTo(Category::class);
+    }
+
+    public function responsable()
+    {
+        return $this->belongsTo(User::class, 'responsable_id');
     }
 
 }
