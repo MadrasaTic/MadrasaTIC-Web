bde206631ca8caef77e97dbb51233eef2f4af055
habs <abenchikh6@gmail.com>
2022-04-29 17:20:44 +0100

    null array bug

--- resources/views/signalmentsPriority.blade.php
+++ resources/views/signalmentsPriority.blade.php
@@ -77,7 +77,7 @@
                             <tr>
                                 <th class="" scope="row">{{$priority['id']}}</th>
                                 <td>{{ $priority['name'] }}</td>
-                                <td>{{ $priority->category['name'] }}</td>
+                                <td>{{ $priority->category['name'] ?? '/' }}</td>
                                 <td>
                                     <a href={{'signalmentsPriority/'.$priority['id']}} class="modify--button me-3 fw-bold">Modifier</a>
                                     <a href={{"/signalmentsPriority/delete/".$priority['id']}} class="remove--button link-danger fw-bold">Supprimer</a>
