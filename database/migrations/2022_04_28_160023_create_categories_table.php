c7eb44fb73694e1be0de49b8f8c7c01572669471
habs <abenchikh6@gmail.com>
2022-04-29 04:44:23 +0100

    Migration table slight change

--- database/migrations/2022_04_28_160023_create_categories_table.php
+++ database/migrations/2022_04_28_160023_create_categories_table.php
@@ -18,6 +18,7 @@ class CreateCategoriesTable extends Migration
             $table->string('name');
             $table->integer('parent_id')->nullable();
             $table->integer('priority_default');
+            $table->json('services_associated')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
         });
