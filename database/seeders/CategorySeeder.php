400c33a94462bb52adaec531bfbffa9bb002310d
Yacine <my.baghdadli@esi-sba.dz>
2022-06-09 16:50:10 +0100

    Added Small Changes

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,32 +16,32 @@ class CategorySeeder extends Seeder
     {
         $Category = Category::create([
             'name' => 'Catégorie A',
-            'parent_id' => null,
-            'priority_default' => 20,
+            //'parent_id' => null,
+            //'priority_default' => 20,
             'description' => 'La description de la categorie A, les filles sont catégorie B, et C, ceci est une déscription statique',
             //'service_id' => null,
         ]);
 
         $Category = Category::create([
             'name' => 'Catégorie B',
-            'parent_id' => 1,
-            'priority_default' => 10,
+            //'parent_id' => 1,
+            // 'priority_default' => 10,
             'description' => 'La description de la categorie B, fille de catégorie A',
             // 'service_id' => null,
         ]);
 
         $Category = Category::create([
             'name' => 'Catégorie C',
-            'parent_id' => 1,
-            'priority_default' => 10,
+            //'parent_id' => 1,
+            //'priority_default' => 10,
             'description' => 'La description de la categorie C, fille de catégorie A',
             // 'service_id' => null,
         ]);
  
         $Category = Category::create([
             'name' => 'Catégorie D',
-            'parent_id' => 2,
-            'priority_default' => 10,
+            //'parent_id' => 2,
+            // 'priority_default' => 10,
             'description' => 'La description de la categorie D, fille de catégorie B',
             // 'service_id' => null,
         ]);
