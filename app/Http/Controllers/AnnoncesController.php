304fcc6dc464de7275a0e373503649b4040e32c3
Yassine <ya.latreche@esi-sba.dz>
2022-06-17 20:02:11 +0100

    deleting public from add and modify functions

--- app/Http/Controllers/AnnoncesController.php
+++ app/Http/Controllers/AnnoncesController.php
@@ -17,15 +17,15 @@ class AnnoncesController extends Controller
      */
 
     public function index(){
-        
+
         $annonces = Annonce::all();
-        //$annonces->beginTime->addDays(10); 
-        //dd($annonces);        
+        //$annonces->beginTime->addDays(10);
+        //dd($annonces);
         return view('annonces' , compact('annonces'));
-        
-        
-        
-        
+
+
+
+
     }
 
     public function show($id)
@@ -37,35 +37,34 @@ class AnnoncesController extends Controller
 
     public function Add(Request $request)
     {
-        
+
         $annonce= new Annonce;
         $annonce->user_id = $request->user()->id;
         $annonce->title = $request->title;
         $annonce->description = $request->description;
         $annonce->beginDate = $request->beginDate;
         $annonce->endDate = $request->endDate;
-        $annonce['public'] = 1;
-        
-          
+
+
         if ($request->hasfile('image')) {
               $image = $request->file('image');
-  
+
                   $path = $image->getClientOriginalName();
                   $image->move(public_path().'/images/annonces', $path);
-                  $annonce->image= $path;                    
+                  $annonce->image= $path;
               }
-           
+
            $annonce->save();
            //dd($annonce);
           return redirect('annonces');
     }
-       
+
     public function edit($id)
     {
         $data = Annonce::find($id);
         return view('modifyAnnonce' , compact('data'));
     }
-    
+
     public function update(Request $request)
     {
         $data = Annonce::findOrFail($request->id);
@@ -74,13 +73,12 @@ class AnnoncesController extends Controller
         $data->description = $request->description;
         $data->beginDate = $request->beginDate;
         $data->endDate = $request->endDate;
-        $data['public'] = 1;
         if ($request->hasfile('image')) {
             $image = $request->file('image');
 
                 $path = $image->getClientOriginalName();
                 $image->move(public_path().'/images/annonces', $path);
-                $data->image= $path;                    
+                $data->image= $path;
             }
         //dd($data);
         $data->save();
