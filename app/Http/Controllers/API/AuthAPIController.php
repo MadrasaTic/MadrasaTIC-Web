a2f53a255e22e71e4d342d72d2d1263e04a06815
Yassine <ya.latreche@esi-sba.dz>
2022-06-19 09:27:55 +0100

    fixing login redirect

--- app/Http/Controllers/API/AuthAPIController.php
+++ app/Http/Controllers/API/AuthAPIController.php
@@ -91,7 +91,7 @@ class AuthAPIController extends Controller
         //dd($user->token);
         $token = $userCreated->createToken('token-name')->plainTextToken;
 	// return "<script> function me(){window.location.replace('exp://'+document.getElementById('ip_address').value+':19000/--/user_token/".$token."');}</script>ip address<input id='ip_address' type='text'><button onclick='me()'>redirect</button>".$token;
-        return redirect()->to('madrasatic-mobile://user_token/'.$token);
+        return redirect()->to('exp://exp.host/@community/with-webbrowser-redirect/--/user_token/'.$token);
     }
 
     /**
